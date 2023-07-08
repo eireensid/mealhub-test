9d9b5e3bf6d59f3679cbc01325f0fe8e6a58669e
Irina <sidoren-irina@yandex.ru>
2023-07-08 21:11:26 +0300

    Add pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button
+      :class="{'': currentPage == pageNumber}"
+      @click="$emit('change', pageNumber)"
+      v-for="pageNumber in totalPages"
+      :key="pageNumber">
+      {{ pageNumber }} 
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  totalPages: Number,
+  currentPage: Number,
+});
+
+defineEmits(["change"]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <table>
+  <table v-if="comments">
     <thead>
         <tr>
             <th @click="sort">id</th>
@@ -8,21 +8,42 @@
         </tr>
     </thead>
     <tbody>
-        <tr v-for="comment in data" :key="comment.id">
+        <tr v-for="comment in comments" :key="comment.id">
             <td>{{comment.id}}</td>
             <td>{{comment.name}}</td>
             <td>{{comment.email}}</td>
         </tr>
     </tbody>
-</table>
+  </table>
+  <pagination @change="refetchComments" :totalPages="totalPages" :currentPage="page" />
 </template> 
 
 <script setup>
-  const { data, error } = await useFetch('https://jsonplaceholder.typicode.com/comments?_page=1')
+  const page = ref(1)
+  const totalPages = ref(0)
   const isIdsAsc = ref(true)
+  const comments = ref(null)
+
+  const fetchComments = async () => {
+    const limit = 15
+    const url = `https://jsonplaceholder.typicode.com/comments?_page=${page.value}&_limit=${limit}`
+    comments.value = await fetch(url)
+      .then(resp => {
+        totalPages.value = Math.ceil(+(resp.headers.get('x-total-count')) / limit)
+        return resp.json()
+      })
+      .catch((error) => error.data)
+  } 
+
+  fetchComments()
+
+  const refetchComments = async pageNumber => {
+    page.value = pageNumber
+    fetchComments()
+  } 
 
   const sort = () => {
-    data.value.sort((comment1, comment2) => {
+    comments.value.sort((comment1, comment2) => {
       return isIdsAsc.value ? comment2.id - comment1.id : comment1.id - comment2.id
     })
 
