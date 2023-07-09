3f6f04089cb5d1939719d3f48b6aff7910c1c82b
Irina <sidoren-irina@yandex.ru>
2023-07-09 13:46:13 +0300

    Decomposition

--- pages/comments/[id].vue
+++ pages/comments/[id].vue
@@ -1,19 +1,3 @@
 <template>
-  <div>
-    <p>{{ comment.name }}</p>
-    <p>{{ comment.email }}</p>
-    <p>{{ comment.body }}</p>
-  </div>
+  <Comment />
 </template>
-
-<script setup>
-  const route = useRoute()
-  const url = `${API_URL}/comments/${route.params.id}`
-  const comment = await fetch(url)
-    .then(resp => {
-      return resp.json()
-    })
-    .catch((error) => error.data)
-
-
-</script>
